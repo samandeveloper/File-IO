90a0f989767c3f8868871a70d8f83b5daecbfcb9
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2020-10-07 12:00:08 -0600

    Update File IO.py
    commit IO description

--- File IO.py
+++ File IO.py
@@ -16,13 +16,14 @@ with open ('test.txt', mode='r+') as my_file:
 	text=my_file.write('hey it\'s me')
 	print(text)
 
-#create a file & write in it
+#create file sad2.txt in the same folder we have our files and write in it.
 with open ('./sad2.txt', mode='w') as my_file:
 	text=my_file.write(':(')
 	print(text)
 
 #file IO Errors
 try:
+	#write in a sad.txt file, in one folder before our main folder.
 	with open ('../sad.txt', mode='r') as my_file:
 		print('my_file.read()')
 except FileNotFoundError as err:
